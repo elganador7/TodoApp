2a1c6662a78a2c6b85ffe7d7b8446c7c78846cdc
elganador7 <cameron.i.springer7@gmail.com>
2022-03-27 18:04:30 -0400

    Adding API's and user sign in

--- functions/index.js
+++ functions/index.js
@@ -4,8 +4,20 @@ const functions = require('firebase-functions');
 const app = require('express')();
 
 const {
-    getAllTodos
+    getAllTodos,
+    postOneTodo,
+    deleteTodo,
+    editTodo
 } = require('./APIs/todos')
 
+const {
+    loginUser
+} = require('./APIs/users')
+
 app.get('/todos', getAllTodos);
+app.post('/todo', postOneTodo);
+app.delete('/todo/:todoId', deleteTodo);
+app.put('/todo/:todoId', editTodo);
+
+app.post('/login', loginUser);
 exports.api = functions.https.onRequest(app);
